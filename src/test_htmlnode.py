907850fc6f85bc95a04f4a1a63b5a582440b89e4
clementine-tw <tsaiyouchen@gmail.com>
2025-05-24 12:58:28 +0800

    add class LeafNdoe for no children node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_tag(self):
@@ -33,6 +33,22 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("a", "this is link", None, {"href": "http"})
         want = "tag: a\nvalue: this is link\nprops: {'href': 'http'}\nchildren: None\n"
         got = repr(node)
+        self.assertEqual(got, want)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_link(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        want = '<a "href"="https://www.google.com">Click me!</a>'
+        got = node.to_html()
+        self.assertEqual(got, want)
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "raw text")
+        want = "raw text"
+        got = node.to_html()
         self.assertEqual(got, want)
 
 if __name__ == "__main__":
